15723b3903e8e7c845761697f2e16e62e53a30d7
Solskyddscenter <Info@solskyddscenter.com>
2026-07-07 22:21:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="kundpris-app-v5-egna-material";const FILES=["./","./index.html","./manifest.json","./icon.svg"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
